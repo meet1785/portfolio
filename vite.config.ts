13c1359b18cd1488acf6cf081184a4c207046471
meet1785 <1032220338@tcetmumbai.in>
2025-02-22 15:49:46 +0530

    Update deployment with correct asset paths

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base : './',
   plugins: [react()],
+  build:{
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
     
